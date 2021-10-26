c754ce8c86012f3f90f7690d826b2962e7ba0268
SamuelDAlencar <sarl.2001@hotmail.com>
2021-10-26 16:08:52 -0300

    general attempts

--- script.js
+++ script.js
@@ -25,7 +25,32 @@ input.addEventListener('keydown', function (event) {
   }
 })
 
+let clearButton = document.getElementById('apaga-tudo')
+
+clearButton.addEventListener('click', function () {
+  for (let i = document.getElementsByTagName('li').children.length; i === document.getElementsByTagName('li').children.length; i--) {
+    document.getElementsByTagName('li')[i].remove();
+  }
+})
+
 ol.addEventListener('click', function (event) {
+  for (let i = 0; i < ol.children.length; i++) {
+    ol.children[i].classList.remove('selectedTask')
+  }
+
   event.target.classList.add('selectedTask')
 })
 
+let undo = 0
+
+ol.addEventListener('dblclick', function (event) {
+  if (undo === 0) {
+    event.target.classList.add('completed')
+
+    undo = 1
+  } else if (undo === 1) {
+    event.target.classList.remove('completed')
+
+    undo = 0
+  }
+})
