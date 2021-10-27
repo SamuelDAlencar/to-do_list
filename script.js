23eddea55c2027a6b8f5adfc99203019136d1745
SamuelDAlencar <sarl.2001@hotmail.com>
2021-10-27 10:44:49 -0300

    styles addition

--- script.js
+++ script.js
@@ -3,46 +3,54 @@ const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 
 button.addEventListener('click', function () {
-  const liCreation = document.createElement('li');
-
-  ol.appendChild(liCreation);
-  liCreation.innerText = input.value;
-
-  input.value = '';
-});
-
-input.addEventListener('keydown', function (event) {
-  if (event.keyCode === 13) {
     const liCreation = document.createElement('li');
 
     ol.appendChild(liCreation);
     liCreation.innerText = input.value;
 
     input.value = '';
-  }
+});
+
+input.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+        const liCreation = document.createElement('li');
+
+        ol.appendChild(liCreation);
+        liCreation.innerText = input.value;
+
+        input.value = '';
+    }
 });
 
 const finishedRemovalButton = document.querySelector('#remover-finalizados');
 finishedRemovalButton.addEventListener('click', function () {
-  const completedTasks = document.querySelectorAll('.completed');
-  for (const task of completedTasks) {
-    task.remove();
-  }
+    const completedTasks = document.querySelectorAll('.completed');
+    for (const task of completedTasks) {
+        task.remove();
+    }
 });
 
 const clearButton = document.getElementById('apaga-tudo')
 clearButton.addEventListener('click', function () {
-  document.getElementsByTagName('ol')[0].innerHTML = '';
+    document.getElementsByTagName('ol')[0].innerHTML = '';
 });
 
 ol.addEventListener('click', function (event) {
-  for (let i = 0; i < ol.children.length; i+= 1) {
-    ol.children[i].classList.remove('selectedTask');
-  }
+    if (event.target === document.querySelector('#lista-tarefas')) {
 
-  event.target.classList.add('selectedTask');
+    } else {
+        for (let i = 0; i < ol.children.length; i += 1) {
+            ol.children[i].classList.remove('selectedTask');
+        }
+
+        event.target.classList.add('selectedTask');
+    }
 });
 
 ol.addEventListener('dblclick', function (event) {
-  event.target.classList.toggle('completed');
+    if (event.target === document.querySelector('#lista-tarefas')) {
+
+    } else {
+        event.target.classList.toggle('completed');
+    }
 });
